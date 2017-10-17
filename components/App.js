342ef1beb0c0021d4afc13d5e93e7a009f71cb1c
mgrobelny <michalmaciejg@gmail.com>
2017-10-17 11:01:03 +0200

    init

---
+++ components/App.js
@@ -0,0 +1,43 @@
+var contacts = [
+  {
+    id: 1,
+    firstName: 'Jan',
+    lastName: 'Nowak',
+    email: 'jan.nowak@example.com',
+  },
+  {
+    id: 2,
+    firstName: 'Adam',
+    lastName: 'Kowalski',
+    email: 'adam.kowalski@example.com',
+  },
+  {
+    id: 3,
+    firstName: 'Zbigniew',
+    lastName: 'Kozioł',
+    email: 'zbigniew.koziol@example.com',
+  },
+  {
+    id: 4,
+    firstName: 'Jacek',
+    lastName: 'Szmit',
+    email: 'jacek.szmit@gmail.com',
+  }
+];
+
+var contactForm = {
+  firstName: '',
+  lastName: '',
+  email: ''
+};
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div className={'app'}>
+        <ContactForm contact={contactForm} />
+        <Contacts items={contacts} />
+      </div>
+    );
+  }
+});
